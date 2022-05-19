574ec283c6f92641c01ac45af0078c58c8240f15
Ivan Ulanov <ivan.ulanov@rtlabs.ru>
2022-05-19 14:30:12 +0300

    02_10b

---
+++ Collections/src/main/java/com/linkedin/collections/Room.java
@@ -0,0 +1,89 @@
+package com.linkedin.collections;
+
+public class Room {
+
+	private String name;
+
+	private String type;
+
+	private int capacity;
+
+	private double rate;
+
+	public Room(String name, String type, int capacity, double rate) {
+		this.name = name;
+		this.type = type;
+		this.capacity = capacity;
+		this.rate = rate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public double getRate() {
+		return rate;
+	}
+
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+
+	@Override
+	public String toString() {
+		return "Room [name=" + name + ", type=" + type + ", capacity=" + capacity + ", rate=" + rate + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(rate);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Room other = (Room) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(rate) != Double.doubleToLongBits(other.rate))
+			return false;
+		return true;
+	}
+	
+	
+
+}
