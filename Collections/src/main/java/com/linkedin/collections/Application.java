1d7f69ccdbbe6e30e837d8843f081c9866a1c435
Ivan Ulanov <ivan.ulanov@rtlabs.ru>
2022-05-27 12:09:04 +0300

    09_02b challenge

--- Collections/src/main/java/com/linkedin/collections/Application.java
+++ Collections/src/main/java/com/linkedin/collections/Application.java
@@ -1,38 +1,30 @@
 package com.linkedin.collections;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Application {
 
 	public static void main(String[] args) {
 
 		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
+		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
 		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+		Room oxfordGuest = new Room("Oxford", "Guest Room", 5, 225.0);
+		Room victoria = new Room("Victoria", "Suite", 5, 225.0);
+		
+		List<Room> rooms = new ArrayList<>(List.of(piccadilly, oxfordGuest, cambridge, victoria, oxford));
 
-		Guest john = new Guest("John", "Doe", false);
-		Guest maria = new Guest("Maria", "Doe", true);
-
-		Map<Room, Guest> assignments = new HashMap<>();
-		assignments.put(oxford, maria);
-		assignments.put(piccadilly, john);
-
-		//Set<Map.Entry<Room, Guest>> collectionView = assignments.entrySet();
-
-		for(Map.Entry<Room, Guest> entry : assignments.entrySet()) {
-			Room keyRoom = entry.getKey();
-			Guest valueGuest = entry.getValue();
-			System.out.printf("%s : %s%n", keyRoom.getName(), valueGuest.getFirstName());
-		}
-		System.out.println();
-				
+		// sort list by natural order
+		//Collections.sort(rooms, Comparator.naturalOrder());
+		rooms.sort(Comparator.naturalOrder());
+		
+		rooms.stream()
+			.forEach(r -> System.out.format("%-15s %-15s %-10f %n", r.getName(), r.getType(), r.getRate()));
+	
+		
 	}
 
 }
-
-
-
-
-
-
--- Collections/src/main/java/com/linkedin/collections/Room.java
+++ Collections/src/main/java/com/linkedin/collections/Room.java
@@ -1,98 +1,98 @@
 package com.linkedin.collections;
 
-public class Room {
+public class Room implements Comparable<Room>{
+    private String name;
+    private String type;
+    private int capacity;
+    private double rate;
+    private boolean petFriendly;
 
-	private String name;
+    public Room(String name, String type, int capacity, double rate) {
+        this.name = name;
+        this.type = type;
+        this.capacity = capacity;
+        this.rate = rate;
+    }
 
-	private String type;
+    public String getName() {
+        return name;
+    }
 
-	private int capacity;
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	private double rate;
-	
-	private boolean petFriendly;
+    public String getType() {
+        return type;
+    }
 
-	public Room(String name, String type, int capacity, double rate) {
-		this.name = name;
-		this.type = type;
-		this.capacity = capacity;
-		this.rate = rate;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public int getCapacity() {
+        return capacity;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public double getRate() {
+        return rate;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
 
-	public int getCapacity() {
-		return capacity;
-	}
+    public boolean isPetFriendly() {
+        return petFriendly;
+    }
 
-	public void setCapacity(int capacity) {
-		this.capacity = capacity;
-	}
+    public void setPetFriendly(boolean petFriendly) {
+        this.petFriendly = petFriendly;
+    }
 
-	public double getRate() {
-		return rate;
-	}
+    @Override
+    public String toString() {
+        return "Room [name=" + name + ", type=" + type + ", capacity=" + capacity + ", rate=" + rate + "]";
+    }
 
-	public void setRate(double rate) {
-		this.rate = rate;
-	}
-	
-	public boolean isPetFriendly() {
-		return petFriendly;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(rate);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 
-	public void setPetFriendly(boolean petFriendly) {
-		this.petFriendly = petFriendly;
-	}
+    @Override
+    public boolean equals(Object obj) {
 
-	@Override
-	public String toString() {
-		return "Room [name=" + name + ", type=" + type + ", capacity=" + capacity + ", rate=" + rate + "]";
-	}
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Room other = (Room) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (Double.doubleToLongBits(rate) != Double.doubleToLongBits(other.rate))
+            return false;
+        return true;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(rate);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-				
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Room other = (Room) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (Double.doubleToLongBits(rate) != Double.doubleToLongBits(other.rate))
-			return false;
-		return true;
-	}
-
+    @Override
+    public int compareTo(Room o) {
+        int result = this.getName().compareTo(o.getName());
+        return result;
+    }
 }
