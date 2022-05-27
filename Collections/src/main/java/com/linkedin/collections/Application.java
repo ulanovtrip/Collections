1643beb1842fc7dc8aaac8d969d7e24cf7d7c9c7
Ivan Ulanov <ivan.ulanov@rtlabs.ru>
2022-05-27 17:03:56 +0300

    08_04b

--- Collections/src/main/java/com/linkedin/collections/Application.java
+++ Collections/src/main/java/com/linkedin/collections/Application.java
@@ -1,38 +1,43 @@
 package com.linkedin.collections;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Application {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
-		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+        Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
+        Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
+        Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+        Room oxford1 = new Room("Oxford", "Guest Room", 5, 225.0);
+        Room victoria = new Room("Victoria", "Suite", 5, 225.0);
 
-		Guest john = new Guest("John", "Doe", false);
-		Guest maria = new Guest("Maria", "Doe", true);
+        List<Room> rooms = new ArrayList<>(List.of(piccadilly, oxford1, cambridge, victoria, oxford));
+        // sorting
+        rooms.sort(Room.RATE_COMPARATOR);
 
-		Map<Room, Guest> assignments = new HashMap<>();
-		assignments.put(oxford, maria);
-		assignments.put(piccadilly, john);
+        // need sorted list
+        int index = Collections.binarySearch(rooms, victoria, Room.RATE_COMPARATOR);
+        System.out.println("index = " + index);
 
-		//Set<Map.Entry<Room, Guest>> collectionView = assignments.entrySet();
+        // new object
+        Room newLondon = new Room("NewLondon", "Suite", 7, 300.0);
 
-		for(Map.Entry<Room, Guest> entry : assignments.entrySet()) {
-			Room keyRoom = entry.getKey();
-			Guest valueGuest = entry.getValue();
-			System.out.printf("%s : %s%n", keyRoom.getName(), valueGuest.getFirstName());
-		}
-		System.out.println();
-				
-	}
+        // return negative
+        int index2 = Collections.binarySearch(rooms, newLondon, Room.RATE_COMPARATOR);
+        System.out.println("index2 = " + index2);
 
+        // add newLondon in list
+        rooms.add(Math.abs(++index), newLondon);
+
+        // find minimum and max by rate
+        System.out.println("Minimum = " + Collections.min(rooms, Room.RATE_COMPARATOR).getName());
+        System.out.println("Maximum = " + Collections.max(rooms, Room.RATE_COMPARATOR).getName());
+
+        System.out.format("%n%n--List Contents--%n");
+        rooms.stream()
+                .forEach(r -> System.out.format("%-15s %-15s %-10f %n", r.getName(), r.getType(), r.getRate()));
+    }
 }
-
-
-
-
-
-
